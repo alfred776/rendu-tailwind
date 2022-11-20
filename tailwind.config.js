20273b7b512063285d3ded686021824ace7c5cd2
alfred776 <117074788+alfred776@users.noreply.github.com>
2022-11-20 21:55:40 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      abc: ['Fraunces','serif'],
+      cde: ['Barlow','sans-serif'],
+    }
+    
+  },
+  plugins: [],
+  screens: {
+    'tablet': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'laptop': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    'desktop': '1280px',
+    // => @media (min-width: 1280px) { ... }
+  },
+}
